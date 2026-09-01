5866c5cff58b4165ca767d81cf1c55213abce2de
Farina Riaz <farinariaz573@gmail.com>
2026-09-01 15:55:20 +0500

    feat: migrate portfolio home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import Navbar from "../src/components/Navbar";
+import FirstPart from "../src/old-pages/FirstPart";
+import SecondPart from "../src/old-pages/SecondPart";
+import ThirdPart from "../src/old-pages/ThirdPart";
+import FourthPart from "../src/old-pages/FourthPart";
+
 export default function Home() {
   return (
-    <main>
-      <h1>My Portfolio</h1>
-    </main>
+    <div className="bg-gradient-to-r from-fuchsia-100 to-purple-100 scroll-smooth">
+      <Navbar />
+
+      <div id="home">
+        <FirstPart />
+      </div>
+
+      <div id="about">
+        <SecondPart />
+      </div>
+
+      <div id="work">
+        <ThirdPart />
+      </div>
+
+      <div id="contact">
+        <FourthPart />
+      </div>
+    </div>
   );
 }
